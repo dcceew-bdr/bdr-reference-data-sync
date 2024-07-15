ac52e4c27b293e7fbadab50c9626fb60cad888ca
Ashley Sommer <ashleysommer@gmail.com>
2024-07-15 12:35:27 +1000

    Output an nquads file for each build Catalog, where each Catalog URI is a named graph in the Vocabs repo. This is for easier loading/updating into the Vocabs db.

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,3 +1,5 @@
+
+
 if __name__ == '__main__':
     import sys
     print("Run with python main.py or python -m src")
@@ -5,10 +7,9 @@ if __name__ == '__main__':
 
 from . import config
 from .catalog import build_catalog
-
+from .voc_graph import make_voc_graph
 
 async def build_catalogs():
-
     catalog_defs = config.get_value("catalogs", None)
     if catalog_defs is None or len(catalog_defs) == 0:
         raise Exception("No catalogs defined")
@@ -17,8 +18,16 @@ async def build_catalogs():
         if 'token' not in catalog_def:
             raise RuntimeError("Catalog entry does not have name property.")
         print(f"building {catalog_def['token']}")
-        catalog_g = await build_catalog(catalog_def)
-
+        cat_details = await build_catalog(catalog_def)
+        cat_ds = make_voc_graph(multigraph=True)
+        cat_uri = cat_details.cat_uri
+        for (s, p, o) in cat_details.graph:
+            cat_ds.add((s, p, o, cat_uri))
+        for content_graph in cat_details.content_graphs:
+            for (s, p, o) in content_graph.graph:
+                cat_ds.add((s, p, o, cat_uri))
+        with open(f"./generated/{catalog_def['token']}/all.nq", "wb") as f:
+            cat_ds.serialize(f, format="nquads")
 
 def entrypoint() -> int:
     import asyncio
--- src/voc_graph.py
+++ src/voc_graph.py
@@ -8,16 +8,25 @@ TERN = rdflib.Namespace("https://w3id.org/tern/ontologies/tern/")
 ABIS = rdflib.Namespace("https://linked.data.gov.au/def/abis/")
 BDR = rdflib.Namespace("https://linked.data.gov.au/dataset/bdr/")
 
+
 @dataclass
 class VocabGraphDetails:
     graph: rdflib.Graph
     keywords: List[str]
     themes: List[rdflib.URIRef]
     token: str
-    vocab_uri: rdflib.URIRef  # This is the ConceptScheme
+    vocab_uri: rdflib.URIRef  # This is the SKOS:ConceptScheme
 
 
-def make_voc_graph():
+@dataclass
+class CatalogGraphDetails:
+    graph: rdflib.Graph
+    token: str
+    cat_uri: rdflib.URIRef  # This is the DCAT:Catalog
+    content_graphs: List[VocabGraphDetails]
+
+
+def make_voc_graph(multigraph: bool = False):
     g = rdflib.Graph(bind_namespaces="core")
     ns = g.namespace_manager
     ns.bind("skos", SKOS)
@@ -29,4 +38,8 @@ def make_voc_graph():
     ns.bind("schema", SDO)
     ns.bind("abis", ABIS)
     ns.bind("bdr", BDR)
+    if multigraph:
+        ds = rdflib.Dataset(store=g.store)
+        ds.namespace_manager = ns
+        return ds
     return g
